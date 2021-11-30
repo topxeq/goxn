15429693ba183526cd8fa658e7be798b9df1554b
unknown <topxeq@163.com>
2021-11-30 15:19:08 +0800

    2021113001

--- cmd/go.mod
+++ cmd/go.mod
@@ -16,3 +16,9 @@ replace github.com/topxeq/goxn v0.0.0 => ../
 replace github.com/topxeq/qlang v0.0.0 => ../../qlang
 
 replace github.com/topxeq/tk v0.0.0 => ../../tk
+
+replace github.com/topxeq/sqltk v0.0.0 => ../../sqltk
+
+replace github.com/topxeq/text v0.0.0 => ../../text
+
+replace github.com/topxeq/charlang v0.0.0 => ../../charlang
--- cmd/goxn.go
+++ cmd/goxn.go
@@ -1,19 +1,115 @@
 package main
 
 import (
+	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/topxeq/goxn"
 	"github.com/topxeq/tk"
 )
 
+var muxG *http.ServeMux
+var portG = ":80"
+var basePathG = "."
+
+func doWms(res http.ResponseWriter, req *http.Request) {
+	if res != nil {
+		res.Header().Set("Access-Control-Allow-Origin", "*")
+		res.Header().Set("Access-Control-Allow-Headers", "*")
+		res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	}
+
+	if req != nil {
+		req.ParseForm()
+	}
+
+	reqT := tk.GetFormValueWithDefaultValue(req, "wms", "")
+	tk.Pl("RequestURI: %v", req.RequestURI)
+
+	if reqT == "" {
+		if tk.StartsWith(req.RequestURI, "/wms") {
+			reqT = req.RequestURI[4:]
+		}
+	}
+
+	tmps := tk.Split(reqT, "?")
+	if len(tmps) > 1 {
+		reqT = tmps[0]
+	}
+
+	if tk.StartsWith(reqT, "/") {
+		reqT = reqT[1:]
+	}
+
+	var paraMapT map[string]string
+	var errT error
+
+	vo := tk.GetFormValueWithDefaultValue(req, "vo", "")
+
+	if vo == "" {
+		paraMapT = tk.FormToMap(req.Form)
+	} else {
+		paraMapT, errT = tk.MSSFromJSON(vo)
+
+		if errT != nil {
+			res.Write([]byte(tk.ErrStrf("操作失败：%v", "invalid vo format")))
+			return
+		}
+	}
+
+	tk.Pl("[%v] REQ: %#v (%#v)", tk.GetNowTimeStringFormal(), reqT, paraMapT)
+
+	toWriteT := ""
+
+	fcT := tk.LoadStringFromFile(filepath.Join(basePathG, "wms", reqT+".gox"))
+	if tk.IsErrStr(fcT) {
+		res.Write([]byte(tk.ErrStrf("操作失败：%v", tk.GetErrStr(fcT))))
+		return
+	}
+
+	// paraMapT["_reqHost"] = req.Host
+	// paraMapT["_reqInfo"] = fmt.Sprintf("%#v", req)
+
+	toWriteT, errT = goxn.RunScriptOnHttp(fcT, res, req, paraMapT["input"], nil, paraMapT, "-base="+basePathG)
+
+	if errT != nil {
+		res.Header().Set("Content-Type", "text/html; charset=utf-8")
+		res.Write([]byte(tk.ErrStrf("操作失败", tk.GetErrStr(errT.Error()))))
+		return
+	}
+
+	if toWriteT == "TX_END_RESPONSE_XT" {
+		return
+	}
+
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	res.Write([]byte(toWriteT))
+}
+
 func main() {
-	tk.Pl("running %v ...", os.Args[1])
 
-	resultT, errT := goxn.RunScript(tk.LoadStringFromFile(os.Args[1]), tk.GetSwitch(os.Args, "-input="), os.Args, nil)
+	portG = tk.GetSwitch(os.Args, "-port=", portG)
+	basePathG = tk.GetSwitch(os.Args, "-dir=", basePathG)
 
-	tk.CheckErrf("error: %v", errT)
+	muxG = http.NewServeMux()
 
-	tk.Pl("%v", resultT)
+	muxG.HandleFunc("/wms/", doWms)
+	muxG.HandleFunc("/wms", doWms)
+
+	tk.PlNow("try starting server on %v（basePath: %v）...", portG, basePathG)
+
+	err := http.ListenAndServe(portG, muxG)
+
+	if err != nil {
+		tk.PlNow("failed to start: %v", err)
+	}
+
+	// resultT, errT := goxn.RunScript(tk.LoadStringFromFile(os.Args[1]), tk.GetSwitch(os.Args, "-input="), os.Args, nil)
+
+	// tk.CheckErrf("error: %v", errT)
+
+	// tk.Pl("%v", resultT)
 
 }
